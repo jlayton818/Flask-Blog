93a3efe83c4fec837b541cc4898a5cddd73c9175
jlayton818 <jdl5gd@umsystem.edu>
2024-11-01 15:56:42 -0600

    Final part completed. Deleting posts works

--- app.py
+++ app.py
@@ -108,4 +108,26 @@ def edit(id):
     #if GET display page
     return render_template('edit.html', post=post)
 
+#create a route to delete a post
+#Delete page will only be processed with a POST request
+#The post id is the url parameter
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    #get the post
+    post = get_post(id)
+    
+    #connect to database
+    conn = get_db_connection()
+    
+    #execute a delete query
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    #commit and close the connection
+    conn.commit()
+    conn.close()
+    #flash a success message
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    #redirect to the homepage
+    return redirect(url_for('index'))
+
 app.run(port=5008)
